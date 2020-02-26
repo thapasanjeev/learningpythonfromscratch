6842b757ceef8d2e7854c227ba95f69a2d1e6722
Sanjeev Thapa <sam.gaha@icloud.com>
2020-02-26 23:14:20 +0545

    latest changes

---
+++ chapter1/chap1_04.py
@@ -0,0 +1,42 @@
+# raw strings
+
+print(r'new line 1 \n new line 2')
+# output: new line 1 \n new line 2
+# r prints line as it is
+
+# printing emojis
+# reference url: https://unicode.org/emoji/charts/full-emoji-list.html
+
+print("\U0001F606 is a face")
+
+
+#python as calculator using print function
+print(2+3)
+# output: 5
+
+# operator for using calculation in python
+# + = addition
+# - = substraction
+# * = multiplication
+# / = division
+# % = modulous or remainder
+# **= exponent or power of
+
+print(2+2)
+print(3-2)
+print(2*3)
+print(9/3) #floating point division means it gives answer in decimals.
+print(9//3) # integer division with no decimals.
+print(4%3)
+print(2**2)
+
+# output
+# 4
+# 1
+# 6
+# 3.0 
+# 3
+# 1
+# 4
+
+
