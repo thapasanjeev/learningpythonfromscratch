d85e6f6fb10d1da624de0e284e9e513e6bc53b64
Sanjeev Thapa <sam.gaha@icloud.com>
2020-02-26 22:47:49 +0545

    first commit

---
+++ chapter1/chap1_01.py
@@ -0,0 +1,26 @@
+# print() function is used to print something on the screen
+
+print('hello sanjeev') #output: hello sanjeev
+
+
+# Strings
+# character inside enclosed single quotes '' or double quotes " " are called strings.
+print("hello sanjeev")  #output: hello sanjeev
+
+#python data types 
+# string: they are character enclosed with single quotes or double quotes.
+
+print('I am a string closed with single quotes') 
+# output: I am a string closed with single quotes
+print("I am a string closed with double quotes") 
+# output: I am a string closed with double quotes
+
+# using double quotes and single quotes in a same string
+print('i read a book called "strings" yesterday') 
+# output: i read a book called "strings" yesterday
+print("I read a book called 'strings' yesterday")
+#output: i read a book called 'strings' yesterday
+print("I'm a string")
+#output: I'm a string
+
+
---
+++ chapter1/chap1_03.py
@@ -0,0 +1,28 @@
+# Escape sequence as normal text
+
+
+print('New line 1 \\n Newline 2')
+print('New line 1 \\t Newline 2')
+print('this is double backslash \\\\')
+print("\\\" \\\'")
+# output: New line 1 \n Newline 2
+# output: New line 1 \t Newline 2
+# output: this is double backslash \\
+# \" \' 
+#\' = '
+#\\ = \
+# \\\' = \'
+
+# Exercise
+# ***********print these***************
+# this is \\ backslash
+# these are  /\/\/\/\ mountains
+# I love   momo(using escape sequence)
+# \" \n \t \'
+
+# solution
+
+print('this is \\\\ backslash')
+print('these are /\\/\\/\\/\\ mountains')
+print('I love \tmomo')
+print('\\\" \\n \\t \\\'')
