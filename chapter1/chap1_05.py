5f4becd6673e7a2babd73c59d727ebe86005146e
Sanjeev Thapa <sam.gaha@icloud.com>
2020-02-27 13:48:28 +0545

    lesson 5 added

---
+++ chapter1/chap1_05.py
@@ -0,0 +1,23 @@
+# variable
+
+# variable simply stores data. data is assigned in variable.
+# example.
+number1 = 3 #here the value 3 is assigned to number1. 
+print(number1) #output: 3
+# operater (=) is an assign operater rather than equal.
+
+#variable can be altered, reassigned or updated by replacing original
+#value. This is why it is called variable so that it can vary.
+
+#previously value of number1 was assigned 3 but we can change the value.
+number1 = 5
+print(number1) #output: 5
+
+#any data types can be assigned to a variable.
+name = "sanjeev"
+number2 = 98980
+float_number= 8.987
+#data type can be changed by reassigning value to a variable.
+name = 456
+#such changing one data type to another data type is known as dynamic programmin language
+print(name) #output: 456
